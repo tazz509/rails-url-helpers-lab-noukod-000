ad7c1c955660065a05f668c6a3ff4e83ac66a894
carly <carly@noukod.com>
2019-01-22 20:17:24 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,20 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    @student=set_student
+  end
+
+  def activate
+    current_status=set_student.active
+    @student=set_student
+    @student.active=!current_status
+    @student.save
+    redirect_to student_path(@student)
   end
 
   private
